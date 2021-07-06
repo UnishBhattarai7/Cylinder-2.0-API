60d88f078529211beab18246d9c12120dd0f2642
AnishNepal <anishnepal62@gmail.com>
2021-07-06 18:36:37 +0545

    Member login

--- Routes/Member_route.js
+++ Routes/Member_route.js
@@ -3,9 +3,8 @@ const router2 = express.Router();
 const Member = require('../Models/Member');
 const bcryptjs = require('bcryptjs');
 const {check, validationResult} = require('express-validator');
-
 const date = require('date-and-time');
-
+const jwt = require('jsonwebtoken');
 
 router2.post('/admin/profile/addMember',[
     check('Firstname', 'Enter First Name').not().isEmpty(),
@@ -39,7 +38,7 @@ async function(req,res)
         .then(function (userDetails) {
             if (userDetails === null) {
                 const data = new Member({Firstname : firstname, Lastname : lastname, Status : status, Phonenumber : phonenumber,
-                    Address : address, Comission : comission, Username : username, Password : hpassword })
+                    Address : address, Comission : comission, Username : username, Password : hpassword, accountCreated: accountCreated })
                data.save()
                .then(function(result)
                {
@@ -61,6 +60,79 @@ async function(req,res)
     {
         res.status(400).json(errors.array());
     }
+})
+
+router2.post('/login',[
+    check('Username', 'Enter Username').not().isEmpty(),
+    check('Password', 'Enter Password').not().isEmpty(),
+],function(req,res)
+{
+    Member.findOne({Username:req.body.Username})
+    
+    .then(function(memberDetails){
+        if(memberDetails === null)
+        {
+            return res.status(401).json({message: "Unauthorised Member !!!"})
+        }
+        bcryptjs.compare(req.body.Password, memberDetails.Password,function(err,cresult)
+        {
+            if(cresult == false)
+            {
+                return res.status(401).json({message: "Username or Password Incorrect."})
+            }
+            if(memberDetails.isfirst == true)
+            {
+                return res.status(401).json({message: "Please change your default password.", success: false})
+            }
+            const token = jwt.sign({MemberID : memberDetails._id}, 'secretkey')
+
+            return res.status(200).json({message: "Login Success", token:token, Usertype : memberDetails.Usertype, success:true})
+            
+            /* Member.updateOne({Username: req.body.Username})
+            .then(async function(res)
+            {
+                await Member.updateOne({Username: req.body.Username},{$set:{isfirst:false}}, {new:true})
+                
+            })
+            .catch(function(e){
+                res.status(500).json({message: e})
+            }) */
+        })
+    })
+    .catch(function(e){
+        res.status(500).json({message: e})
+    })
+})
+
+router2.put('/changepassword', async function (req, res) {
+
+    const username = req.body.Username
+    const password = req.body.Password
+    const Npassword = await bcryptjs.hash(req.body.Npassword, 10)
+
+    Member.findOne({ Username: username })
+        .then(function (memberDetails) {
+            if (memberDetails === null) {
+                return res.status(401).json({ message: "Invalid Username", success: false })
+            }
+            bcryptjs.compare(password, memberDetails.Password, function (err, result) {
+                if (result === false) {
+                    return res.status(401).json({ message: "Username or Password does not match."})
+                }
+                else
+                {
+                    Member.findOneAndUpdate({Username : username}, 
+                        {Password : Npassword, isfirst:false})
+                    .then(function(result){
+                        res.status(200).json({success : true, message : "Password Changed."})
+                    })
+                    .catch(function(err){
+                        res.status(500).json({message : err})
+                    })
+                }
+            })
+        })
+        .catch()
 })
 
 module.exports = router2;
--- Routes/User_route.js
+++ Routes/User_route.js
@@ -42,56 +42,6 @@ function(req,res)
     }
 })
 
-router1.post('/login', function(req,res)
-{
-    User.findOne({Phonenumber:req.body.Phonenumber})
-    .then(function(userDetails)
-    {
-        if(userDetails === null)
-        {
-            return res.status(401).json({message: "Unauthorised User !!!"})
-        }
-        bcryptjs.compare(req.body.Password, userDetails.Password,function(err,cresult)
-        {
-            if(cresult == false)
-            {
-                return res.status(401).json({message: "Password Incorrect."})
-            }
-            const token = jwt.sign({UserID : userDetails._id}, 'secretkey')
 
-            res.status(200).json({message: "Login Success", token:token, Usertype : userDetails.Usertype, success:true})
-        })
-    })
-    .catch()
-})
-
-router1.put('/changepassword', async function (req, res) {
-
-    const Phonenumber = req.body.Phonenumber
-    const Password = req.body.Password
-    const Npassword = await bcryptjs.hash(req.body.Npassword, 10)
-
-    User.findOne({ Phonenumber: Phonenumber })
-        .then(function (userDetails) {
-            if (userDetails === null) {
-                return res.status(401).json({ message: "Invalid Username", success: false })
-            }
-            bcryptjs.compare(Password, userDetails.Password, function (err, result) {
-                if (result === false) {
-                    return res.status(401).json({ message: "Phone number and Password Do not Matched." })
-                }else{
-                    User.findOneAndUpdate({Phonenumber : Phonenumber}, 
-                        {Password : Npassword})
-                    .then(function(result){
-                        res.status(200).json({success : true, message : "Password Changed."})
-                    })
-                    .catch(function(err){
-                        res.status(500).json({message : err})
-                    })
-                }
-            })
-        })
-        .catch()
-})
 
 module.exports = router1;
