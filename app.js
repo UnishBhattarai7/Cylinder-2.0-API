ab61b4f520617bacc00e29119e88753f62d47a1a
bhanubhakta56 <abromeo114@gmail.com>
2021-07-12 21:43:13 +0545

    path for member route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const bodyParser = require('body-parser');
 const db = require('./Database/db');
 const user_route = require('./Routes/User_route')
+const member_route = require('./Routes/Member_route')
 const company_route = require('./Routes/Company_route')
 
 const reseller_route = require('./Routes/Reseller_route')
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(user_route);
+app.use(member_route);
 app.use(company_route);
 app.use(reseller_route);
 
