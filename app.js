53c680bed9e9df3f9052866ddab6476f557cfa06
AnishNepal <anishnepal62@gmail.com>
2021-07-12 13:41:00 +0545

    Company Details

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const bodyParser = require('body-parser');
 const db = require('./Database/db');
 const user_route = require('./Routes/User_route')
+const company_route = require('./Routes/Company_route')
 const cors = require('cors')
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(user_route);
+app.use(company_route);
 
 app.listen(90);
 
