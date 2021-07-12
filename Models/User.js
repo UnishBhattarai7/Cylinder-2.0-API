5d3eb6225f73ac2d3e5912bc292674d63bae8c64
bhanubhakta56 <abromeo114@gmail.com>
2021-07-12 21:42:01 +0545

    user and member model

---
+++ Models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User',{
+    Fullname : {
+        type : String,
+        required : true
+    },
+    Phonenumber : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    Password : {
+        type : String,
+        required : true,
+        firsttime : true
+    },
+    Usertype : {
+        type : String,
+        enum : ['Admin','Employee']
+    }
+})
+
+module.exports = User;
