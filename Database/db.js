793aef0f94db6f2c7249dbc3c321bcb1b40732ff
sabin <csabinji@gmail.com>
2021-08-25 19:04:51 +0545

    Port configuration

--- Database/db.js
+++ Database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/cylinder',{
+mongoose.connect('mongodb+srv://sabin:sabin@cluster0.jjzgy.mongodb.net/cylinder',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology : true
