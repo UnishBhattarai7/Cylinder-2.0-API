edef41e9e26a8b630421d03a3ea10a9d48468a4f
sabin <csabinji@gmail.com>
2021-08-25 20:42:50 +0545

    new changes

--- Database/db.js
+++ Database/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/cylinder',{
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology : true
+});
+
+mongoose.connection.on('connected', () => {
+    consolee.log('Mongoose is connected !!!');
 })
